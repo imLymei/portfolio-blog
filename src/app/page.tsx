77fedb8bf43b90c17ab472e645c9d62e3b01bdfe
Felipe Cardoso <lymeicontato@gmail.com>
2024-06-17 17:51:59 -0300

    chore: update home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
-    <section className="flex flex-col gap-6">
+    <section className="flex flex-col gap-6 text-lg">
       <h1 className="py-24 text-center text-6xl font-bold">
         {"<👋 HELLO WORLD />"}
       </h1>
       <p>
-        My name is Felipe Cardoso and I am a full stack developer focused on web
-        development that likes to build new and old things, just like this
-        portfolio with a blog! So <span>✨ O L D S C H O O L ✨</span>!
-      </p>
-      <p>
-        Jokes apart, I like to build everything that exists to expand my
-        knowledge, and try to create new things to challenge myself.
+        My name is Felipe Cardoso and I am a full stack web developer that likes
+        to build old and new things to practice and expand my knowledge, just
+        like this portfolio with a blog! So ✨ C O O L ✨! Im currently working
+        as the leader of development at Jean Piaget, where i develop tools and
+        apps that help all the employees and students. I also run this blog
+        where i post all my experiences of a mid-level developer that handles
+        productions of software and a small team.
       </p>
     </section>
   );
