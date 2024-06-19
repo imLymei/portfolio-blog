2b593f10e5f38e5955c1806d76fe0b8e15e61ab4
Felipe Cardoso <lymeicontato@gmail.com>
2024-06-19 08:20:05 -0300

    style: edit `code` tag style

--- src/app/blog/[...slug]/layout.tsx
+++ src/app/blog/[...slug]/layout.tsx
@@ -4,7 +4,7 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="prose max-w-none lg:prose-lg dark:prose-invert">
+    <div className="prose max-w-none lg:prose-lg dark:prose-invert prose-code:rounded prose-code:bg-neutral-800 prose-code:p-1 prose-code:font-mono prose-code:before:content-[''] prose-code:after:content-['']">
       {children}
     </div>
   );
